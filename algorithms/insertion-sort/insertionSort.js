0a397cf0c5772b864f0c0feb5c02e2970fed4a7c
GitSaranga <bsaranga@gmail.com>
2021-03-16 23:15:46 +0530

    insertion sort

---
+++ algorithms/insertion-sort/insertionSort.js
@@ -0,0 +1,17 @@
+let arr = [2,9,4,3,7,5,8,1,0]
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let hold = arr[i]
+        let k = i - 1
+        while(k >= 0 && arr[k] > hold) {
+            arr[k + 1] = arr[k]
+            arr[k] = hold
+            k--
+        }
+    }
+
+    return arr
+}
+
+console.log(insertionSort(arr))
